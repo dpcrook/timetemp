77b73eb47ded356fbeda3b16932d5a5a84f78e03
David Crook <dpcrook@users.noreply.github.com>
2018-04-17 18:08:26 -0600

    toward using a config file for device addresses and secret keys

--- install/weather_logging_sparkfun.py
+++ install/weather_logging_sparkfun.py
@@ -1,35 +1,15 @@
-#!/usr/bin/python
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
 
-# Google Spreadsheet BMP Sensor Data-logging Example
+#  - read indoor-located pressure/temperature sensor
+#    - log sensor data to a phant server
+#    - log external temperature data
 
-# Depends on the 'gspread' package being installed.  If you have pip installed
-# execute:
-#   sudo pip install gspread
-
-# Copyright (c) 2014 Adafruit Industries
-# Author: Tony DiCola
-
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
 import sys
 import time
 import datetime
 import json
+from pprint import pprint
 import ssl
 
 import Adafruit_BMP.BMP085 as BMP085
@@ -38,38 +18,64 @@ from Adafruit_LED_Backpack.SevenSegment import SevenSegment
 from phant import Phant
 
 LOGGING = True
+LOGGING = False
 COUNT = 0
 
 # How long to wait (in seconds) between measurements.
-FREQUENCY_SECONDS      = 300
+FREQUENCY_SECONDS = 300
 
 # How long to wait (in seconds) to display F or C.
-ALTERNATE_TEMP_SCALE_SECONDS      = 5
+ALTERNATE_TEMP_SCALE_SECONDS = 5
 
 # Approximately how often measurements are made (in seconds)
-MEASUREMENT_INTERVAL =  2 * ALTERNATE_TEMP_SCALE_SECONDS
+MEASUREMENT_INTERVAL = 2 * ALTERNATE_TEMP_SCALE_SECONDS
 
 # How seldom to upload the sensor data, if LOGGING is on
-COUNT_INTERVAL =  FREQUENCY_SECONDS / MEASUREMENT_INTERVAL
+COUNT_INTERVAL = FREQUENCY_SECONDS / MEASUREMENT_INTERVAL
 
-
-# Create sensor instance with default I2C bus (On Raspberry Pi either 0 or
-# 1 based on the revision, on Beaglebone Black default to 1).
+# Create sensor instance with default I2C bus
 bmp = BMP085.BMP085(mode=BMP085.BMP085_HIGHRES, address=0x77)
 
 # Initialize a LED display
-#segment = SevenSegment(address=0x70)
-segment = SevenSegment(address=0x71)
-print segment
+segment = SevenSegment(address=0x70)
+#segment = SevenSegment(address=0x71)
+print(segment)
 
 # Read in Phant config
-json_keys_file2 = 'data.crookster.org.json'
-p2=Phant(jsonPath=json_keys_file2)
+if LOGGING:
+    json_keys_file2 = 'data.crookster.org.json'
+    p2 = Phant(jsonPath=json_keys_file2)
+
+    print(
+        'Logging sensor measurements taken every {2} seconds to "{0}" every {1} seconds.'
+    ).format(p2.title, FREQUENCY_SECONDS, MEASUREMENT_INTERVAL)
+    print(p2)
+
+print('Press Ctrl-C to quit.')
+
+with open('weather_logging_config.json') as config_file:
+    config = json.loads(config_file.read())
+
+pprint(config)
 
 
-print 'Logging sensor measurements taken every {2} seconds to "{0}" every {1} seconds.'.format(p2.title, FREQUENCY_SECONDS, MEASUREMENT_INTERVAL)
-print p2
-print 'Press Ctrl-C to quit.'
+def convert_json_string_to_hexadecimal_value(s):
+    value = 0
+    try:
+        value = int(s, 16)
+    except:
+        pass
+    return value
+
+
+bmp_address = convert_json_string_to_hexadecimal_value(
+    config["i2c_addresses"]["bmp085"])
+led_display_address = convert_json_string_to_hexadecimal_value(
+    config["i2c_addresses"]["i2c_led"])
+print(bmp_address)
+print(led_display_address)
+
+raise SystemExit
 
 while True:
 
@@ -77,42 +83,47 @@ while True:
 
     try:
 
-	# Attempt to get sensor readings.
-	temp = bmp.read_temperature()
-	pressure = bmp.read_pressure()
-	altitude = bmp.read_altitude()
+        # Attempt to get sensor readings.
+        temp = bmp.read_temperature()
+        pressure = bmp.read_pressure()
+        altitude = bmp.read_altitude()
 
         temp_in_F = (temp * 9.0 / 5.0) + 32.0
 
-        print "Temperature: %.2f C" % temp
-        print "Temperature: %.2f F" % temp_in_F
-        print "Pressure:    %.2f hPa" % (pressure / 100.0)
-        print "Altitude:    %.2f m" % altitude
-        print "Press CTRL+C to exit"
-        print ""
+        print("BMP Sensor")
+        print("  Temp(°C): %.1f C" % temp)
+        print("  Temp(°F): %.1f F" % temp_in_F)
+        print("Pressure:    %.2f hPa" % (pressure / 100.0))
+        print("Altitude:    %.2f m" % altitude)
+        print("Press CTRL+C to exit")
+        print("")
 
         for display_tmp_in_F in [False, True]:
 
             if display_tmp_in_F:
 
                 if round(temp_in_F * 10.0) < 1000.0:
-                    segment.set_digit(0, int(round(temp_in_F) / 10))     # Tens
-                    segment.set_digit(1, int(round(temp_in_F) % 10))          # Ones
-                    segment.set_digit(2, int(int(round(temp_in_F * 10.0)) % 10))   # Tenth
+                    segment.set_digit(0, int(round(temp_in_F) / 10))  # Tens
+                    segment.set_digit(1, int(round(temp_in_F) % 10))  # Ones
+                    segment.set_digit(2, int(
+                        int(round(temp_in_F * 10.0)) % 10))  # Tenth
                     segment.set_digit(3, 'F')
                     segment.set_colon(True)
                 else:
-                    segment.set_digit(0, int(round(temp_in_F) / 100))     # Hundreds
-                    segment.set_digit(1, int(round(temp_in_F - 100.0) / 10))      # Tens
-                    segment.set_digit(2, int(round(temp_in_F) % 10))      # Ones
+                    segment.set_digit(0,
+                                      int(round(temp_in_F) / 100))  # Hundreds
+                    segment.set_digit(1, int(
+                        round(temp_in_F - 100.0) / 10))  # Tens
+                    segment.set_digit(2, int(round(temp_in_F) % 10))  # Ones
                     segment.set_digit(3, 'F')
                     segment.set_colon(False)
 
             else:
                 # write degrees
-                segment.set_digit(0, int(round(temp) / 10))     # Tens
-                segment.set_digit(1, int(round(temp) % 10))          # Ones
-                segment.set_digit(2, int(int(round(temp * 10.0)) % 10))   # Tenth
+                segment.set_digit(0, int(round(temp) / 10))  # Tens
+                segment.set_digit(1, int(round(temp) % 10))  # Ones
+                segment.set_digit(2,
+                                  int(int(round(temp * 10.0)) % 10))  # Tenth
                 segment.set_digit(3, 'C')
                 segment.set_colon(True)
 
@@ -126,16 +137,19 @@ while True:
             ambient_temp_F = temp_in_F
             ambient_pressure = pressure / 100.0
 
-            fields = (ambient_pressure, ambient_temp_C, ambient_temp_F, altitude)
-            print fields
+            fields = (ambient_pressure, ambient_temp_C, ambient_temp_F,
+                      altitude)
+            print(fields)
 
             if (COUNT % COUNT_INTERVAL) == 0:
-                p2.log(altitude, ambient_pressure, ambient_temp_C, ambient_temp_F)
-	        print 'Wrote a row to {0}'.format(p2.title)
-                print(p2.remaining_bytes, p2.cap)
+                p2.log(altitude, ambient_pressure, ambient_temp_C,
+                       ambient_temp_F)
+                print('Wrote a row to {0}'.format(p2.title))
+                print((p2.remaining_bytes, p2.cap))
             else:
-	        print 'at {0} seconds out of {1}'.format((COUNT * MEASUREMENT_INTERVAL) % FREQUENCY_SECONDS, FREQUENCY_SECONDS )
-
+                print('at {0} seconds out of {1}'.format(
+                    (COUNT * MEASUREMENT_INTERVAL) % FREQUENCY_SECONDS,
+                    FREQUENCY_SECONDS))
 
         COUNT = COUNT + 1
 
@@ -146,7 +160,7 @@ while True:
             error_tables['KI'] = 1
         else:
             error_tables['KI'] += 1
-        print error_tables
+        print(error_tables)
         sys.exit(0)
 
     except ssl.SSLError:
@@ -157,7 +171,7 @@ while True:
             error_tables['ssl.SSLError'] += 1
         segment.clear()
         segment.write_display()
-        print error_tables
+        print(error_tables)
 
 #    except:
 #        print "unhandled exception, skipping"
@@ -170,4 +184,4 @@ while True:
     finally:
         segment.clear()
         segment.write_display()
-        print error_tables
+        print(error_tables)
